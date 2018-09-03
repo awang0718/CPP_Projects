2fcba705a12016c2b137241be09fc52a2472f499
Andrew Wang <awang57@linux-a3.ews.illinois.edu>
2018-09-03 01:50:55 -0500

    file submission

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -9,12 +9,32 @@ using cs225::HSLAPixel;
 
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+  PNG inputImage = PNG();  // Create input image
+  inputImage.readFromFile(inputFile);  // Read in PNG image from inputFile
+
+  PNG outputImage = PNG(inputImage.width(), inputImage.height());  // Create output image with same dimensions as input image
+
+  for (unsigned x = 0; x < inputImage.width(); x++) {
+    for (unsigned y = 0; y < inputImage.height(); y++) {
+      HSLAPixel & inputPixel = inputImage.getPixel(x, y);  // Retrieve pixel from inputImage at location (x, y)
+      HSLAPixel & outputPixel = outputImage.getPixel(outputImage.width()-1-x, outputImage.height()-1-y);  // Retrieve pixel from inputImage at location (outputImage.width()-1-x, outputImage.height()-1-y)
+      
+      // Set the outputPixel equal to the inputPixel by setting the outputPixel's hue, saturation, luminance, and alpha value equal to inputPixel's hue, saturation, luminance, and alpha value equal to
+      outputPixel.h = inputPixel.h;
+      outputPixel.s = inputPixel.s;
+      outputPixel.l = inputPixel.l;
+      outputPixel.a = inputPixel.a;
+    }
+  }
+
+  outputImage.writeToFile(outputFile); // Write output Image to outputFule
 }
+
 
 PNG myArt(unsigned int width, unsigned int height) {
   PNG png(width, height);
   // TODO: Part 3
+  
 
   return png;
 }
