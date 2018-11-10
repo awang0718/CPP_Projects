3b788f5417f2456fed097631d0501fad895dbf7b
Andrew Wang <awang57@eceb-2022-13.ews.illinois.edu>
2018-11-09 18:45:37 -0600

    file submission

--- mp6/dsets.cpp
+++ mp6/dsets.cpp
@@ -1,1 +1,34 @@
 /* Your code here! */
+#include"dsets.h"
+
+void DisjointSets::addelements(int num) {
+  for(int i = 0;i < num; i++)
+    data.push_back(-1);
+}
+
+int DisjointSets::find(int elem) {
+  if(data[elem] < 0)
+    return elem;
+  else{
+    int root = find(data[elem]);
+    data[elem] = root;
+    return root;
+  }
+}
+
+void DisjointSets::setunion(int a, int b) {
+  int idxa = find(a), idxb = find(b);
+  if(idxa == idxb) return;
+  if(data[idxa] <= data[idxb]){
+    data[idxa] += data[idxb];
+    data[idxb] = idxa;
+  }
+  else {
+    data[idxb] += data[idxa];
+    data[idxa] = idxb;
+  }
+}
+
+int DisjointSets::size(int elem) {
+  return -data[find(elem)];
+}
--- mp6/dsets.h
+++ mp6/dsets.h
@@ -1,1 +1,20 @@
 /* Your code here! */
+#ifndef DESTS_H
+#define DESTS_H
+
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+class DisjointSets{
+  public:
+    void addelements(int num);
+    int find(int elem);
+    void setunion(int a, int b);
+    int size(int elem);
+  // private:
+    vector<int> data;
+};
+
+#endif
